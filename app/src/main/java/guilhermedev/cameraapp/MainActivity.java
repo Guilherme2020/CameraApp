b5bbe1220a38e689ce04949a8e8818d8e653e9c9
guilherme2020 <guilhermerodrigues123477@gmail.com>
2016-05-02 09:09:51 -0300

    CameraApp

---
+++ app/src/main/java/guilhermedev/cameraapp/MainActivity.java
@@ -0,0 +1,36 @@
+package guilhermedev.cameraapp;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    Button btn = (Button)findViewById(R.id.btnTirarFoto);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent in = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(in,1);
+
+
+            }
+        });
+    }
+
+    public void getFoto(){
+
+    }
+
+}
